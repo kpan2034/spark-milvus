4ba11b80e1145131fc186f0ae06527d246c0ab06
kpan2034 <keyur.panchal@gmail.com>
2023-12-04 20:17:14 -0500

    Implemented getCollectionStatistics for MilvusClient.

--- src/main/scala/com/milvus/spark/connector/MilvusClient.scala
+++ src/main/scala/com/milvus/spark/connector/MilvusClient.scala
@@ -1,5 +1,10 @@
 package com.milvus.spark.connector
 
+import io.milvus.client.MilvusServiceClient
+import io.milvus.param.collection.GetCollectionStatisticsParam
+import io.milvus.param.{ConnectParam, LogLevel, R}
+import io.milvus.response.GetCollStatResponseWrapper
+
 /*
 This is a wrapper class for the Milvus Java SDK.
 It lets readers of individual partitions use their
@@ -8,6 +13,35 @@ Any RPC error handling should be performed here, and
 TODO: either a more user-friendly error should be returned to the caller
 (or handled here)
  */
-class MilvusClient {
 
+val SuccessCode = R.Status.Success.getCode
+
+class MilvusClient(host: String, port:Int) {
+  private lazy val client = {
+    val milvusClient = new MilvusServiceClient(ConnectParam.newBuilder.withHost(host).withPort(port).build)
+    milvusClient.setLogLevel(LogLevel.Error)
+    milvusClient
+  }
+  def getCollectionStatistics(collectionName: String) = {
+    val param = GetCollectionStatisticsParam.newBuilder.withCollectionName(collectionName).build
+    val response = client.getCollectionStatistics(param)
+    if (response.getStatus != SuccessCode) {
+      println(response.getMessage)
+      -1
+    }
+    val wrapper = new GetCollStatResponseWrapper(response.getData)
+    wrapper.getRowCount
+  }
+}
+
+object MilvusClient {
+  private val DEFAULT_HOST = "localhost"
+  private val DEFAULT_PORT = 19530
+  def apply() = {
+    new MilvusClient(DEFAULT_HOST, DEFAULT_PORT)
+  }
+
+  def apply(host: String, port: Int) = {
+    new MilvusClient(host, port)
+  }
 }
